4a667497d769ef73cfdf9fb0c4d1cb85ad980005
brumocas <brucosta2002@gmail.com>
2024-12-09 22:07:03 +0000

    Update

--- Projects/Proj2_Yolo/dataset.py
+++
@@ -1,7 +0,0 @@
-# Import the dataset
-from roboflow import Roboflow
-rf = Roboflow(api_key="dnZcw1fNasJT5SaFbDdG")
-project = rf.workspace("vortexbuoytrainingset").project("buoy-detection-qzjg1")
-version = project.version(1)
-dataset = version.download("yolov11")
-                
